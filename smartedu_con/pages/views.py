7f62056a0884358f40037e6985cf0b02a8ddb8fe
mabacioglu <mabacioglu@gmail.com>
2021-09-27 18:41:26 +0300

    Lesson03

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>INDEX SAYFASI<H1>")
+
+    return render(request,"index.html")
 
 # Create your views here.
